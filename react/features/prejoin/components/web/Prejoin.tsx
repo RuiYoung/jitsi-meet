ed8fad289b47f6deb849b0e5532906cbc14bc5ea
ruiyoung <714021656@qq.com>
2024-03-20 14:02:12 +0800

    feat: 还原展示操作

--- react/features/prejoin/components/web/Prejoin.tsx
+++ react/features/prejoin/components/web/Prejoin.tsx
@@ -216,11 +216,11 @@ const Prejoin = ({
 }: IProps) => {
     const showDisplayNameField = useMemo(
         () => isDisplayNameVisible && !readOnlyName,
-        [ isDisplayNameVisible, readOnlyName ]);
+        [isDisplayNameVisible, readOnlyName]);
     const showErrorOnField = useMemo(
         () => showDisplayNameField && showErrorOnJoin,
-        [ showDisplayNameField, showErrorOnJoin ]);
-    const [ showJoinByPhoneButtons, setShowJoinByPhoneButtons ] = useState(false);
+        [showDisplayNameField, showErrorOnJoin]);
+    const [showJoinByPhoneButtons, setShowJoinByPhoneButtons] = useState(false);
     const { classes } = useStyles();
     const { t } = useTranslation();
     const dispatch = useDispatch();
@@ -379,72 +379,72 @@ const Prejoin = ({
 
     return (
         <PreMeetingScreen
-            showDeviceStatus = { deviceStatusVisible }
-            showUnsafeRoomWarning = { showUnsafeRoomWarning }
-            title = { t('prejoin.joinMeeting') }
-            videoMuted = { !showCameraPreview }
-            videoTrack = { videoTrack }>
+            showDeviceStatus={deviceStatusVisible}
+            showUnsafeRoomWarning={showUnsafeRoomWarning}
+            title={t('prejoin.joinMeeting')}
+            videoMuted={!showCameraPreview}
+            videoTrack={videoTrack}>
             <div
-                className = { classes.inputContainer }
-                data-testid = 'prejoin.screen'>
-                {!showDisplayNameField ? (<Input
-                    accessibilityLabel = { t('dialog.enterDisplayName') }
-                    autoComplete = { 'name' }
-                    autoFocus = { true }
-                    className = { classes.input }
-                    error = { showErrorOnField }
-                    id = 'premeeting-name-input'
-                    onChange = { setName }
-                    onKeyPress = { showUnsafeRoomWarning && !unsafeRoomConsent ? undefined : onInputKeyPress }
-                    placeholder = { t('dialog.enterDisplayName') }
-                    readOnly = { readOnlyName }
-                    value = { name } />
+                className={classes.inputContainer}
+                data-testid='prejoin.screen'>
+                {showDisplayNameField ? (<Input
+                    accessibilityLabel={t('dialog.enterDisplayName')}
+                    autoComplete={'name'}
+                    autoFocus={true}
+                    className={classes.input}
+                    error={showErrorOnField}
+                    id='premeeting-name-input'
+                    onChange={setName}
+                    onKeyPress={showUnsafeRoomWarning && !unsafeRoomConsent ? undefined : onInputKeyPress}
+                    placeholder={t('dialog.enterDisplayName')}
+                    readOnly={readOnlyName}
+                    value={name} />
                 ) : (
-                    <div className = { classes.avatarContainer }>
+                    <div className={classes.avatarContainer}>
                         <Avatar
-                            className = { classes.avatar }
-                            displayName = { name }
-                            participantId = { participantId }
-                            size = { 72 } />
-                        {isDisplayNameVisible && <div className = { classes.avatarName }>{name}</div>}
+                            className={classes.avatar}
+                            displayName={name}
+                            participantId={participantId}
+                            size={72} />
+                        {isDisplayNameVisible && <div className={classes.avatarName}>{name}</div>}
                     </div>
                 )}
 
                 {showErrorOnField && <div
-                    className = { classes.error }
-                    data-testid = 'prejoin.errorMessage'>{t('prejoin.errorMissingName')}</div>}
+                    className={classes.error}
+                    data-testid='prejoin.errorMessage'>{t('prejoin.errorMissingName')}</div>}
 
-                <div className = { classes.dropdownContainer }>
+                <div className={classes.dropdownContainer}>
                     <Popover
-                        content = { hasExtraJoinButtons && <div className = { classes.dropdownButtons }>
+                        content={hasExtraJoinButtons && <div className={classes.dropdownButtons}>
                             {extraButtonsToRender.map(({ key, ...rest }) => (
                                 <Button
-                                    disabled = { joiningInProgress || showErrorOnField }
-                                    fullWidth = { true }
-                                    key = { key }
-                                    type = { BUTTON_TYPES.SECONDARY }
-                                    { ...rest } />
+                                    disabled={joiningInProgress || showErrorOnField}
+                                    fullWidth={true}
+                                    key={key}
+                                    type={BUTTON_TYPES.SECONDARY}
+                                    {...rest} />
                             ))}
-                        </div> }
-                        onPopoverClose = { onDropdownClose }
-                        position = 'bottom'
-                        trigger = 'click'
-                        visible = { showJoinByPhoneButtons }>
+                        </div>}
+                        onPopoverClose={onDropdownClose}
+                        position='bottom'
+                        trigger='click'
+                        visible={showJoinByPhoneButtons}>
                         <ActionButton
-                            OptionsIcon = { showJoinByPhoneButtons ? IconArrowUp : IconArrowDown }
-                            ariaDropDownLabel = { t('prejoin.joinWithoutAudio') }
-                            ariaLabel = { t('prejoin.joinMeeting') }
-                            ariaPressed = { showJoinByPhoneButtons }
-                            disabled = { joiningInProgress
+                            OptionsIcon={showJoinByPhoneButtons ? IconArrowUp : IconArrowDown}
+                            ariaDropDownLabel={t('prejoin.joinWithoutAudio')}
+                            ariaLabel={t('prejoin.joinMeeting')}
+                            ariaPressed={showJoinByPhoneButtons}
+                            disabled={joiningInProgress
                                 || (showUnsafeRoomWarning && !unsafeRoomConsent)
-                                || showErrorOnField }
-                            hasOptions = { hasExtraJoinButtons }
-                            onClick = { onJoinButtonClick }
-                            onOptionsClick = { onOptionsClick }
-                            role = 'button'
-                            tabIndex = { 0 }
-                            testId = 'prejoin.joinMeeting'
-                            type = 'primary'>
+                                || showErrorOnField}
+                            hasOptions={hasExtraJoinButtons}
+                            onClick={onJoinButtonClick}
+                            onOptionsClick={onOptionsClick}
+                            role='button'
+                            tabIndex={0}
+                            testId='prejoin.joinMeeting'
+                            type='primary'>
                             {t('prejoin.joinMeeting')}
                         </ActionButton>
                     </Popover>
@@ -452,8 +452,8 @@ const Prejoin = ({
             </div>
             {showDialog && (
                 <JoinByPhoneDialog
-                    joinConferenceWithoutAudio = { joinConferenceWithoutAudio }
-                    onClose = { closeDialog } />
+                    joinConferenceWithoutAudio={joinConferenceWithoutAudio}
+                    onClose={closeDialog} />
             )}
         </PreMeetingScreen>
     );
